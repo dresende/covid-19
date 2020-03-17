8dcf683a15e53dd43ed562d828fdced28b0f98b6
Paulo Querido <paulo@querido.pt>
2020-03-17 17:55:11 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Covid-19 - cálculo de progressão
+
+Script básico para encontrar a taxa diária de crescimento da pandemia Covid-19.
+
+Contém dados de Portugal desde o início da pandemia, 2 de Março 2020, até dia 17 de março.
+
+
+## Instalar
+
+Clonar esta repo.
+
+
+## Usar
+
+1. `cd covid-19`
+2. `python covid-crescimento-pt.py`
+
+
+### variáveis
+**dias**: dias desde 2 de março, data dos primeiros casos em Portugal
+**casos**: número total de casos em cada dia
+**diff**: diferença para a véspera em int
+**diff_porcent**: diferença para a véspera em percentagem
+**projecao**: fórmula 
+
+
+## TO-DO
+
+ * parir também um gráfico
+ * encontrar uma fórmula para corrigir os coeficientes de previsão
+
+
+### Changelog
+
+ - **2020-03-17**: Primeira release
+ 
---
+++ covid-crescimento-pt.py
@@ -0,0 +1,29 @@
+import numpy as np
+#import matplotlib.pyplot as plt
+dias = np.array([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
+casos = np.array([2,4,6,9,13,21,30,39,41,59,78,112,169,245,331,448])
+#print (len(dias), '  dias: ', dias)
+#print (len(casos), ' casos: ', casos)
+
+coef1, coef2 = np.polyfit(dias,np.log(casos),1)
+print ('coeficientes soft e hard', coef1, coef2)
+
+contador = 0
+diff = 0
+diff_porcent = 0
+for dia in dias:
+    if (contador > 0):
+        diff = casos[contador] - casos[contador-1]
+        diff_porcent = (diff / casos[contador-1]) * 100
+
+    projecao = "%.0f" % (np.exp(coef2)*np.exp(coef1*dia))
+
+    print (dia ,' ', casos[contador] , ' diferença: ' + str(diff) , ' taxa de crescimento:',  "%.1f" % diff_porcent  + '%' , ' (projeção:' + str(projecao) + ')' )
+    contador = contador + 1
+
+
+'''
+Projeção básica: aplicar o coeficiente
+'''
+projecao = "%.0f" % (np.exp(coef2)*np.exp(coef1*(dia+1)))
+print (dia + 1, ' ',  str(projecao) + " (projeção)")
